3b356f08745c113419d93e1aee15c26cb3433746
Adam Guest <chamook@live.dk>
2019-02-23 21:52:55 +0100

    Set up a basic my-colours endpoint

--- src/Chamook.Colours/Program.fs
+++ src/Chamook.Colours/Program.fs
@@ -13,9 +13,35 @@ open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.Logging
 open Giraffe
 
+type Colour = {
+    Id   : string
+    Name : string
+    Hex  : string
+    RGB  : RGB
+    HSL  : HSL
+}
+and RGB = { Red : int; Green : int; Blue : int }
+and HSL = { Hue : int; Saturation : int; Lightness : int }
+
+type MyColoursDto = { Colours : Colour list }
+
+let myColours = [
+    { Id   = "abc123"
+      Name = "Red"
+      Hex  = "#FF0000"
+      RGB  = { Red = 255; Green = 0; Blue = 0 }
+      HSL  = { Hue = 0; Saturation = 100; Lightness = 50 } }
+    { Id   = "def456"
+      Name = "Yellow"
+      Hex  = "#FFFF00"
+      RGB  = { Red = 255; Green = 255; Blue = 0 }
+      HSL  = { Hue = 60; Saturation = 100; Lightness = 50 } }
+    ]
+
 let webApp =
     choose [
-        route "/health"   >=> Successful.OK "Everything's fine here, how are you?" ]
+        route "/health"   >=> Successful.OK "Everything's fine here, how are you?"
+        route "/my-colours" >=> Successful.OK { Colours = myColours } ]
 
 let configureApp (app : IApplicationBuilder) =
     app.UseGiraffe webApp
